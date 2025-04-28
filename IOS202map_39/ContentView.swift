ee4a9781c2e1eb8f032f311deae483f71a19bea4
Saikaku <grnmnsta@gmail.com>
2025-04-28 10:43:44 +0900

    Initial Commit

---
+++ IOS202map_39/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS202map_39
+//
+//  Created by cmStudent on 2025/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS202map_39/IOS202map_39App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS202map_39App.swift
+//  IOS202map_39
+//
+//  Created by cmStudent on 2025/04/28.
+//
+
+import SwiftUI
+
+@main
+struct IOS202map_39App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
